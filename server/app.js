6abbd1d8d308446e05555bf9adf8295f38d6b632
Ben <benawhiting@gmail.com>
2023-01-21 12:41:55 -0800

    Set up middleware

--- server/app.js
+++ server/app.js
@@ -12,3 +12,18 @@ const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json('Hello, World!');
+});
+
+app.listen(process.env.PORT, () => console.log(`Listening on Port ${process.env.PORT}!`));
