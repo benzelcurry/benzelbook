6307fabc5498c7ce825d6223e1c328a0b43d36a8
Ben <benawhiting@gmail.com>
2023-01-23 16:55:50 -0800

    Make nav buttons link to respective pages

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,12 +8,14 @@ import '../stylesheets/Nav.css';
 const Nav = () => {
   return (
     <div className='header'>
-      <button className='site-title'>Benzelbook</button>
+      <Link to={'/'} className='nav-link'>
+        <button className='site-title'>Benzelbook</button>
+      </Link>
       { /* MAKE LOGIN BUTTON CONDITIONALLY DISPLAY ONLY IF USER
         IS LOGGED OUT */ }
-      
+      <Link to={'/login'} className='nav-link'>
         <button className='login-btn nav-btn'>Log In</button>
-      
+      </Link>
     </div>
   );
 };
