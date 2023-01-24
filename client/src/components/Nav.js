51727c2a1bdc166f70d8afd494cff8813d657b88
benzelcurry <benawhiting@gmail.com>
2023-01-24 15:04:07 -0800

    Display username and hide login/signup buttons if user is logged in

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,26 @@
 // Nav/Header component
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import '../stylesheets/Nav.css';
 
 const Nav = () => {
+  const [user, setUser] = useState();
+  const token = localStorage.getItem('token');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const body = { token: localStorage.getItem('token') }
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/`, body)
+      .then((response) => {
+        console.log(response);
+        setUser(response.data.username);
+      })
+  }, [token])
+
   return (
     <div className='header'>
       <Link to={'/'} className='nav-link'>
@@ -14,12 +29,20 @@ const Nav = () => {
       { /* MAKE LOGIN BUTTON CONDITIONALLY DISPLAY ONLY IF USER
         IS LOGGED OUT */ }
       <div className="nav-right">
-        <Link to={'/signup'} className='nav-link'>
-          <button className='signup-btn nav-btn'>Sign Up</button>
-        </Link>
-        <Link to={'/login'} className='nav-link'>
-          <button className='login-btn nav-btn'>Log In</button>
-        </Link>
+        { user ?
+          <div>
+            <button className='nav-btn'>{user}</button>
+          </div>
+          : 
+          <div>
+            <Link to={'/signup'} className='nav-link'>
+              <button className='signup-btn nav-btn'>Sign Up</button>
+            </Link>
+            <Link to={'/login'} className='nav-link'>
+              <button className='login-btn nav-btn'>Log In</button>
+            </Link>
+          </div>
+        }
       </div>
     </div>
   );
