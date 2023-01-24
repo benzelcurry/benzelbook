5808c9e7ab3c55ecd1c69436b3ca008f4f6ce607
benzelcurry <benawhiting@gmail.com>
2023-01-24 13:47:16 -0800

    Create sign up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -9,7 +9,23 @@ const SignUp = () => {
     <div>
       <Nav />
       <div className="signup-container">
-        Sign Up stuff will go here.
+        <form action="" method="POST">
+          <div className="signup-inputs">
+            <label htmlFor="first_name">First Name: </label>
+            <input type="text" name='first_name' id='first_name' placeholder='First Name' />
+            
+            <label htmlFor="family_name">Family Name: </label>
+            <input type="text" name='family_name' id='family_name' placeholder='Family Name' />
+            <label htmlFor="username">Username: </label>
+            <input type="text" name='username' id='username' placeholder='Username' />
+            <label htmlFor="password">Password: </label>
+            <input type="password" name='password' id='password' placeholder='Password' />
+            <label htmlFor="confirm_password">Confirm Password: </label>
+            <input type="confirm_password" name='confirm_password' id='confirm_password' placeholder='Confirm Password' />
+          </div>
+
+          <button>Sign Up</button>
+        </form>
       </div>
       <Footer />
     </div>
