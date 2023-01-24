73dbafc0414c33968816e97cbf755b5e1543a731
benzelcurry <benawhiting@gmail.com>
2023-01-24 10:58:02 -0800

    Create basic sign in form

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,17 +1,30 @@
 // Log-In Component
 
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import Nav from './Nav';
 import Footer from './Footer';
 import '../stylesheets/LogIn.css';
 
 const LogIn = () => {
+
+
   return (
     <div>
       <Nav />
       <div className="login-container">
-        Log In stuff will go here!
+        <form action="" method="POST">
+          <div className="form-group">
+            <label htmlFor="username">Username: </label>
+            <input type="text" name="username" id="username" placeholder="Username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input type="password" name="password" id="password" placeholder="Password" />
+          </div>
+          <button>Log In</button>
+        </form>
       </div>
       <Footer />
     </div>
