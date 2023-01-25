fcc076561730b00e08d40958052af1b13c64120d
benzelcurry <benawhiting@gmail.com>
2023-01-25 11:37:44 -0800

    Display user avatar and full name on user page

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Nav from './Nav';
 import Footer from './Footer';
+import DefaultAvatar from '../images/default-avatar.svg';
 import '../stylesheets/User.css';
 
 const User = () => {
+  const [user, setUser] = useState({});
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    const body = { token: localStorage.getItem('token') }
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/`, body)
+      .then((response) => {
+        console.log(response.data);
+        setUser(response.data);
+      })
+  }, [token])
+
   return (
     <div>
       <Nav />
       <div className="user-container">
-        User info will go here.
+        <div className="user-header">
+          <img src={DefaultAvatar} alt="User avatar" className='profile-pic' />
+          <div className="user-basics">
+            <div className="user-fullname">{user.name}</div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
