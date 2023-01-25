a3eed46a176c9939bb596820085c4b5f8a45cde8
benzelcurry <benawhiting@gmail.com>
2023-01-25 14:30:33 -0800

    Pull profile page info from URL params

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,4 +1,7 @@
+// Component for user profile pages
+
 import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import Nav from './Nav';
@@ -7,17 +10,33 @@ import DefaultAvatar from '../images/default-avatar.svg';
 import '../stylesheets/User.css';
 
 const User = () => {
+  const { username } = useParams();
   const [user, setUser] = useState({});
+  const [page, setPage] = useState({});
   const token = localStorage.getItem('token');
+  const navigate = useNavigate();
 
+  // Pulls active user on client end
   useEffect(() => {
     const body = { token: localStorage.getItem('token') }
     axios.post(`${process.env.REACT_APP_SERVER_URL}/`, body)
       .then((response) => {
-        console.log(response.data);
         setUser(response.data);
       })
   }, [token])
+
+  // Pulls account info from URL params to populate page
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/users/${username}`)
+      .then((results) => {
+        if (results.data.user) {
+          setPage(results.data.user); 
+        } else {
+          // REPLACE THIS WITH A 'USER DOES NOT EXIST' PAGE
+          navigate('/');
+        };
+      });
+  }, [navigate, username]);
 
   return (
     <div>
@@ -27,7 +46,7 @@ const User = () => {
         <div className="user-header">
           <img src={DefaultAvatar} alt="User avatar" className='profile-pic' />
           <div className="user-basics">
-            <div className="user-fullname">{user.name}</div>
+            <div className="user-fullname">{page.first_name} {page.family_name}</div>
           </div>
         </div>
         <div className="user-contents">
