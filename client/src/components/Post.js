abd7f1ee6fc9fcd4d9d6e9f5303f9da79d2b6d75
benzelcurry <benawhiting@gmail.com>
2023-01-26 13:23:01 -0800

    Fix sitewide styling bug

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import DefaultAvatar from '../images/default-avatar.svg';
 import '../stylesheets/Post.css';
 
 const Post = ({ content, author }) => {
@@ -14,7 +15,16 @@ const Post = ({ content, author }) => {
   }, [author]);
 
   return (
-    <div className='post-container'>{content}</div>
+    <div className='post-container'>
+      {/* WILL PROBABLY NEED TO UPDATE IMAGE PULLING TO SUCCESSFULLY
+        DISPLAY USER PROFILE PICS; WILL LIKELY NEED TO INSTALL A DEPENDENCY
+        OR SOMETHING LIKE THAT TO HANDLE IMAGE HOSTING? */}
+      <img src={
+        postAuthor.pfp ? postAuthor.pfp : DefaultAvatar
+      } alt="User avatar" className='mini-avatar' />
+      <p className='post-author'>{postAuthor.first_name} {postAuthor.family_name}</p>
+      <p className="post-body">{content}</p>
+    </div>
   );
 };
 
