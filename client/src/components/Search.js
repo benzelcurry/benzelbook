b0fa734144a6bbd35b4be25882d4739fa990b603
Ben <benawhiting@gmail.com>
2023-01-29 14:56:04 -0800

    Make search feature functional

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,8 @@
 // Component for displaying search results
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 import Nav from './Nav';
 import Footer from './Footer';
@@ -9,6 +10,26 @@ import '../stylesheets/Search.css';
 
 const Search = ({ query }) => {
   const location = useLocation();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/users`)
+      .then((response) => {
+        // REMOVE CONSOLE.LOG()'S WHEN SEARCH IS FUNCTIONAL
+        const results = response.data.user_list;
+        if (location.state.query) {
+          const substr = location.state.query.toLowerCase();
+          const refined = results.filter(user => 
+            user.first_name.toLowerCase().includes(substr) ||
+            user.family_name.toLowerCase().includes(substr)
+          );
+          console.log(refined);
+          return setUsers(refined);
+        }
+        console.log(response.data.user_list);
+        return setUsers(response.data.user_list);
+      });
+  }, [location.state.query]);
 
   return (
     <div>
