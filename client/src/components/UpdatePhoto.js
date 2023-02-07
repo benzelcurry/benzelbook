01a9ffa0f4ddc4b91235dd429659a2af3d65c77f
Ben <benawhiting@gmail.com>
2023-02-07 15:08:50 -0800

    Create skeleton component for updating profile photo

---
+++ client/src/components/UpdatePhoto.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Nav from './Nav';
+import Footer from './Footer';
+import '../stylesheets/UpdatePhoto.css';
+
+const UpdatePhoto = () => {
+  return (
+    <div>
+      <Nav />
+      <div>
+        Update your photo!
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default UpdatePhoto;
