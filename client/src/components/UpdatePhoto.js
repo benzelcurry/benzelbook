54f44db2db5ae41a64d8ce5431e488ced49864e8
Ben <benawhiting@gmail.com>
2023-02-07 15:28:35 -0800

    Hide form if user tries accessing avatar update page for someone else

--- client/src/components/UpdatePhoto.js
+++ client/src/components/UpdatePhoto.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 import Nav from './Nav';
 import Footer from './Footer';
 import '../stylesheets/UpdatePhoto.css';
 
 const UpdatePhoto = () => {
+  const { username } = useParams();
+  const [user, setUser] = useState({});
+  const token = localStorage.getItem('token');
+
+  // Pulls active user on client end
+  useEffect(() => {
+    const body = { token: localStorage.getItem('token') }
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/`, body)
+      .then((response) => {
+        setUser(response.data);
+      })
+  }, [token])
+
   return (
     <div>
       <Nav />
-      <div>
-        Update your photo!
+      <div className='update-container'>
+        {
+          user.username === username ?
+          <div>Update stuff will go here</div>
+          :
+          <div className="wrong-user">403 Forbidden: Access Denied</div>
+        }
       </div>
       <Footer />
     </div>
