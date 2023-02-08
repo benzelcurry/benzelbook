c96d896915bfc1554e21a72d9e7b1afb431b5110
benzelcurry <benawhiting@gmail.com>
2023-02-08 15:00:36 -0800

    Link update about form with server

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import Edit from '../images/edit.svg';
 import '../stylesheets/About.css';
@@ -7,12 +9,29 @@ const About = ({ user, page }) => {
   const [edit, setEdit] = useState(false);
   const [about, setAbout] = useState('');
 
+  const navigate = useNavigate();
+
   const handleAbout = (e) => {
     setAbout(e.target.value);
   };
 
   const handleEdit = () => {
     edit ? setEdit(false) : setEdit(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = { about: about };
+    axios.put(`${process.env.REACT_APP_SERVER_URL}/users/${user.id}/about`, body)
+      .then((response) => {
+        console.log(response);
+        if (response.data.message === 'Success') {
+          navigate(0);
+        };
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
   };
 
   return (
@@ -27,11 +46,10 @@ const About = ({ user, page }) => {
       }
       {
         edit ?
-        <form action="">
+        <form action="" onSubmit={(e) => handleSubmit(e)}>
           <textarea name="user-about" id="user-about" className='user-about-text'
             placeholder='Write something about yourself...' maxLength={250}
-            onChange={(e) => handleAbout(e)}>
-            { user.about ? user.about : '' }
+            onChange={(e) => handleAbout(e)} defaultValue={ user.about ? user.about : '' }>
           </textarea>
           <i>{250 - about.length} chars remaining</i>
           <button>Submit Changes</button>
