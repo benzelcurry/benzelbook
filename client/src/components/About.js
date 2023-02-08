ced2462aed944ab64cd65aa90d377f5df70b1647
benzelcurry <benawhiting@gmail.com>
2023-02-08 13:53:44 -0800

    Create skeleton component for About section

---
+++ client/src/components/About.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import Edit from '../images/edit.svg';
+import '../stylesheets/About.css';
+
+const About = ({ user, page }) => {
+  return (
+    <div>
+      About content goes here.
+    </div>
+  );
+};
+
+export default About;
