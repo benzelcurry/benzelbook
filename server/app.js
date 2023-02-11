6e07b7439924038f1859198ec953efd83135e4a8
Ben <benawhiting@gmail.com>
2023-02-11 00:20:05 -0800

    Fix headers

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(cors({ credentials: true }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
