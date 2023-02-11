bc0507710e36b91fb40f207482f7577f82473b1f
Ben <benawhiting@gmail.com>
2023-02-10 23:08:40 -0800

    Fix one more cors issue

--- server/app.js
+++ server/app.js
@@ -24,15 +24,15 @@ const app = express();
 app.use(cors({
   origin: 'https://benzel-book.web.app'
 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.header('*'));
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", req.header('*'));
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   next();
+// });
 app.use(helmet({}));
 app.use(compression());
 app.use(cookieParser());
